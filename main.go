32c642a55ca51c2963e622584e79c95284c52593
Arundhati Das <arundhutidas2000@gmail.com>
2025-10-03 13:07:01 -0400

    Refactor CLI command structure and enhance input handling

--- main.go
+++ main.go
@@ -1,11 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+) 
 
-func cleanInput(text string) []string{
-	return []string{}
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
 }
 
-func main() {
-	fmt.Println("Hello, World!")
+func cleanInput(text string) []string{
+	textLower:= strings.ToLower(text)
+	words:= strings.Fields(textLower)
+	return words
+}
+
+func commandExit() error{
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return  nil
+}
+
+func main(){
+	mapexit:=map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	for{
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() { 
+			break 
+		}
+		text:=scanner.Text()
+		inputstring:=cleanInput(text)
+		if len(inputstring) > 0 {
+			fmt.Println("Your command was:", inputstring[0])
+		} else {
+			fmt.Println("Your command was: <empty>")
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading input:", err)
+	}
 }
--- repl_test.go
+++ repl_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCleanInput(t *testing.T) {
 	cases := []struct {
@@ -8,8 +10,12 @@ func TestCleanInput(t *testing.T) {
 		expected []string
 	}{
 		{
-			input:    "  hello  world  ",
+			input:    " hello  world ",
 			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
 		},
 	}
 	for _, c := range cases {
@@ -17,12 +23,21 @@ func TestCleanInput(t *testing.T) {
 	// Check the length of the actual slice against the expected slice
 	// if they don't match, use t.Errorf to print an error message
 	// and fail the test
+	if len(actual) != len(c.expected) {
+            t.Errorf("Expected length %d, got %d", len(c.expected), len(actual))
+            continue
+        }
 	for i := range actual {
 		word := actual[i]
 		expectedWord := c.expected[i]
 		// Check each word in the slice
 		// if they don't match, use t.Errorf to print an error message
 		// and fail the test
-	}
+		 if word != expectedWord {
+        	t.Errorf("Expected: %s, Actual: %s", expectedWord, word)
+    	} else {
+			t.Logf("PASS: input=%q output=%v", c.input, actual)
+		}
+		}
 	}
 }
